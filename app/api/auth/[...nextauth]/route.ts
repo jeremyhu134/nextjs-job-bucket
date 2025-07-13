1891fe07137835628652b9e25cf799887b1ec5a0
jeremyhu134 <52206356+jeremyhu134@users.noreply.github.com>
2025-07-12 22:19:30 -0400

    1.1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-export const authOptions = {
+const authOptions = {
     providers: [
         GitHub({
             clientId: process.env.AUTH_GITHUB_ID as string,
